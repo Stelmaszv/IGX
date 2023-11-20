31cefa01242a7b3c205edabcefb3ab41bff82b21
Stelmaszv <stelmaszv@gmail.com>
2023-11-20 14:04:19 +0100

    fix router

--- app/public/index.php
+++ app/public/index.php
@@ -1,13 +1,4 @@
-<?php
-require('../vendor/autoload.php');
-
-use App\Core\Route\RouteMatch;
-use App\Infrastructure\DB\Connect;
-
-$connect = Connect::getInstance();
-$routeMatch = new RouteMatch();
-require('../route.php');
-
-$routeMatch->setRoute();
-?>
+<script>
+    window.location.href = "route.php";
+</script>
 
