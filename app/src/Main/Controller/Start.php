38d7254d84c22174fce4d5e3211a144eb8d233b7
Stelmaszv <stelmaszv@gmail.com>
2024-01-18 14:18:53 +0100

    recator

--- app/src/Main/Controller/Start.php
+++ app/src/Main/Controller/Start.php
@@ -2,25 +2,12 @@
 
 namespace App\Main\Controller;
 
-use App\Main\Model\User;
 use App\Core\Controller\AbstractController;
-use App\Main\Entity\UserEntity;
 
 class Start extends AbstractController
 {
     function InitMain() : void
     {   
-        $users = $this->getModel(User::class);
-        $users->add(
-            new UserEntity(
-                'name',
-                'passs',
-                'fqsef@email.com',
-                'update',
-                23
-            )
-        );
-
         $this->setTemplate('../templete/home.html',
             [
             'form' => $this->generateForm([
